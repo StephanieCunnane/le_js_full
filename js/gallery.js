07cc71dac2b9124c18ecca158ca01b8469a979df
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-06-28 01:40:59 +0000

    Add basic thumbnail to main image functionality

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,15 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and then respond appropriately to a click on an image
+
 function activateGallery() {
-  alert('hello from the gallery file!');
+  let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
+  let mainImage = document.querySelector('#gallery-photo img');
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', function() {
+      // Set clicked thumbnail as new main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute('src', newImageSrc);
+    });
+  });
 }
